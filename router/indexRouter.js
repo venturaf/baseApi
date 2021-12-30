56470bf2a3799d4f5a40a184c57c100fb906c7c3
Franner Ventura <franner.ventura@gmail.com>
2021-12-30 17:10:04 -0300

    [id-03-deployHeroku] cambios para tomar el index
    [id-03-deployHeroku] cambios para tomar el index

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -1,4 +1,4 @@
-module.exports = (router, auth, StatusCodes) => {
+module.exports = (router) => {
   
   router.get('/', function(req, res) {
           res.status(StatusCodes.OK).json(" Welcome 🙌 Basic Api 🙌")
